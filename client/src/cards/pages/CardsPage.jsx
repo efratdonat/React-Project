f8043c2cefa331a70f36f8bb2646efa619bb607c
efratdonat <efratdonat@gmail.com>
2023-07-17 20:30:48 +0300

    CHANGE

--- client/src/cards/pages/CardsPage.jsx
+++ client/src/cards/pages/CardsPage.jsx
@@ -1,13 +1,54 @@
+// import { Container } from "@mui/material";
+// import PageHeader from "../../components/PageHeader";
+// import { useEffect, useState } from "react";
+// import CardsFeedback from "../components/CardsFeedback";
+// import useCards from "../hooks/useCards";
+
+// const CardsPage = () => {
+//   const { value, handleGetCards, handleDeleteCard } = useCards();
+//   const { cards, error, isPending } = value;
+//   const [filteredCards, setFilteredCards] = useState([]);
+
+//   useEffect(() => {
+//     handleGetCards();
+//   }, []);
+
+//   const onDeleteCard = async (cardId) => {
+//     await handleDeleteCard(cardId);
+//     await handleGetCards();
+//   };
+
+//   return (
+//     <Container>
+//       <PageHeader
+//         title="Cards"
+//         subtitle="Here you can find business cards from all categories"
+//       />
+
+//       <CardsFeedback
+//         isPending={isPending}
+//         error={error}
+//         cards={cards}
+//         filteredCards={filteredCards}
+//         setFilteredCards={setFilteredCards}
+//         onDelete={onDeleteCard}
+//       />
+//     </Container>
+//   );
+// };
+
+// export default CardsPage;
+
+
 import { Container } from "@mui/material";
 import PageHeader from "../../components/PageHeader";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import CardsFeedback from "../components/CardsFeedback";
 import useCards from "../hooks/useCards";
 
 const CardsPage = () => {
   const { value, handleGetCards, handleDeleteCard } = useCards();
-  const { cards, error, isPending } = value;
-  const [filteredCards, setFilteredCards] = useState([]);
+  const { cards, error, isPending, filteredCards } = value;
 
   useEffect(() => {
     handleGetCards();
@@ -28,9 +69,7 @@ const CardsPage = () => {
       <CardsFeedback
         isPending={isPending}
         error={error}
-        cards={cards}
-        filteredCards={filteredCards}
-        setFilteredCards={setFilteredCards}
+        cards={filteredCards}
         onDelete={onDeleteCard}
       />
     </Container>
